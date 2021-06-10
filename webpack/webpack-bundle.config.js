2a5489d317483bcab965d5e2b38e23d16d15c994
Daniel Hincapie <strike970124@gmail.com>
2021-06-10 09:47:07 -0500

    Update webpack-bundle.config.js

--- webpack/webpack-bundle.config.js
+++ webpack/webpack-bundle.config.js
@@ -58,7 +58,7 @@ module.exports = {
 
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': '"production"'
+        NODE_ENV: JSON.stringify("production"),
       }
     }),
 
